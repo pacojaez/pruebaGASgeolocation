2bd1ecb9445e695c8cf20944982946835f89a21c
Paco Jaez <pacojaez@gmail.com>
2023-11-05 17:40:54 +0100

    Geolocation Init_

---
+++ Código.js
@@ -0,0 +1,16 @@
+  // function doGet() {
+  //   return HtmlService
+  //       .createTemplateFromFile('index')
+  //       .evaluate();
+  // }
+
+  // funcion para abrir la UI
+  function doGet() {
+      var template = HtmlService.createTemplateFromFile("index");
+      return template.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME);
+  }
+
+  function include(filename) {
+      return HtmlService.createHtmlOutputFromFile(filename)
+          .getContent();
+  }
---
+++ geoLocation.js
@@ -0,0 +1,27 @@
+function getLocation() {
+  // console.log('nlocation' + navigator.geolocation.getCurrentPosition())
+  // const x = document.getElementById("output");
+
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(showPosition);
+  //   console.log('nlocation' + navigator.geolocation.getCurrentPosition(showPosition))
+  // } else {
+  //   x.innerHTML = "Geolocation is not supported by this browser.";
+  // }
+
+  // const successCallback = (position) => {
+  // Logger.log(position);
+  // };
+
+  // const errorCallback = (error) => {
+  //   Logger.log(error);
+  // };
+
+  // navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  Logger.log('getLocation')
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
