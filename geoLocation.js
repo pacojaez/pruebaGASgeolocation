bcc796ccba9727e1db5158bbf1ca100118bd79e7
Paco Jaez <pacojaez@gmail.com>
2023-11-05 19:11:49 +0100

    Updated geolocation

--- geoLocation.js
+++ geoLocation.js
@@ -1,27 +1,28 @@
 function getLocation() {
-  // console.log('nlocation' + navigator.geolocation.getCurrentPosition())
-  // const x = document.getElementById("output");
+    // console.log('nlocation' + navigator.geolocation.getCurrentPosition())
+    // const x = document.getElementById("output");
 
-  // if (navigator.geolocation) {
-  //   navigator.geolocation.getCurrentPosition(showPosition);
-  //   console.log('nlocation' + navigator.geolocation.getCurrentPosition(showPosition))
-  // } else {
-  //   x.innerHTML = "Geolocation is not supported by this browser.";
-  // }
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(showPosition);
+    //   console.log('nlocation' + navigator.geolocation.getCurrentPosition(showPosition))
+    // } else {
+    //   x.innerHTML = "Geolocation is not supported by this browser.";
+    // }
 
-  // const successCallback = (position) => {
-  // Logger.log(position);
-  // };
+    // const successCallback = (position) => {
+    // Logger.log(position);
+    // };
 
-  // const errorCallback = (error) => {
-  //   Logger.log(error);
-  // };
+    // const errorCallback = (error) => {
+    //   Logger.log(error);
+    // };
 
-  // navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-  Logger.log('getLocation')
+    // navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    Logger.log('getLocation')
 }
 
+// show the geolocation
 function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
 }
